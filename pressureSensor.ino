13d00cf650311363a317f40ae4306714c5815659
Collin Town <collintown@gmail.com>
2021-11-15 13:20:09 -0500

    Made pump acutally activate when its supposed to!

--- pressureSensor.ino
+++ pressureSensor.ino
@@ -17,70 +17,91 @@ bool pumpRunning = false;
 noDelay printPSI(500, outputPSI);
 noDelay updateControlLoop(100, controlLoop, false);
 
-void outputPSI() {
+void outputPSI()
+{
   Serial.println(getPSI());
 }
 
-void startPump(long time) {
-  if (!pumpRunning) {
+void startPump(long time)
+{
+  if (!pumpRunning)
+  {
     interval = time;
     startTime = millis();
     pumpRunning = true;
+    digitalWrite(pumpPin, LOW);
   }
 }
 
-float getPSI() {
+float getPSI()
+{
   int sensorVal = analogRead(pressurePin);
-  float voltage = (sensorVal * 5.0)/1024.0;
-  float psi = (30*((voltage - 0.45)/(4.5-0.45)));
+  float voltage = (sensorVal * 5.0) / 1024.0;
+  float psi = (30 * ((voltage - 0.45) / (4.5 - 0.45)));
 
   return psi;
 }
 
-void controlLoop() {
-  float psi = getPSI();
-  if (millis() - controlLoopStartTime <= controlLoopInterval) {
+void controlLoop()
+{
+  if (millis() - controlLoopStartTime <= controlLoopInterval)
+  {
     // Control Loop Code Here
-    float targetPSI = (1.0/12.0)*((float)(millis() - controlLoopStartTime)/1000.0);
+    float targetPSI = (1.0 / 12.0) * ((float)(millis() - controlLoopStartTime) / 1000.0);
     float currentPSI = getPSI();
     float error = targetPSI - currentPSI;
 
-    if (abs(error) > 0.4) {
+    if (abs(error) > 0.4)
+    {
       // check if pump active
       // activate pump for 1 second
       startPump(1000);
     }
   }
+  else
+  {
+    updateControlLoop.stop();
+  }
 }
 
-void setup() {
+void setup()
+{
   pinMode(pumpPin, OUTPUT);
   Serial.begin(9600);
   digitalWrite(pumpPin, HIGH);
 }
 
-void loop() {
+void loop()
+{
   printPSI.update();
   updateControlLoop.update();
 
-  if (pumpRunning) {
-    if (millis() - startTime >= interval) {
+  if (pumpRunning)
+  {
+    if (millis() - startTime >= interval)
+    {
       digitalWrite(pumpPin, HIGH);
       pumpRunning = false;
     }
   }
-  if (Serial.available() > 0) {
-    String msg = Serial.readString(); msg.replace("\n",""); msg.toUpperCase();
-    if (msg != "START" && !pumpRunning) {
+  if (Serial.available() > 0)
+  {
+    String msg = Serial.readString();
+    msg.replace("\n", "");
+    msg.toUpperCase();
+    if (msg != "START" && !pumpRunning)
+    {
       float val = msg.toFloat();
-      if (val > 0.0 && val < 5.0) {
+      if (val > 0.0 && val < 5.0)
+      {
         pumpRunning = true;
         startTime = millis();
         interval = (val * 1000);
         digitalWrite(pumpPin, LOW);
       }
     }
-    else {
+    else
+    {
       startPump(1000);
       controlLoopStartTime = millis();
       updateControlLoop.start();
